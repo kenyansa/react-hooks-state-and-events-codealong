c4debc9893280524278fa7075e3666516131bf45
kenyansa <kenyansajustine@gmail.com>
2022-05-10 12:30:59 +0300

    understood state events

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+  //under the handleClick callback function: we must call the setter function to update our state variable.
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+  const color =isOn ? "skyblue" : "white";
+  return <button style={{background: color}} onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
